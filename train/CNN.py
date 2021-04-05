02aad270e2cfac632b272c07ef1d16f19c8cc681
JaroslawBozek <48987078+JaroslawBozek@users.noreply.github.com>
2021-04-05 03:34:28 +0200

    Update CNN.py

--- train/CNN.py
+++ train/CNN.py
@@ -1,84 +1,19 @@
 
 import numpy as np
-import matplotlib.pyplot as plt
+import argparse
 import matplotlib.image as npimg
 import os
 
 ## Keras
 import tensorflow as tf
-from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Convolution2D, MaxPooling2D, Dropout, Flatten, Dense, Lambda, Conv2D, ELU, ReLU, Input
 import cv2
 import pandas as pd
-import random
-import ntpath
 
 ## Sklearn
-from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
-
-datadir = '/home/tasoth/av_ws/src/av_07/data'
-imagedir = 'images/images/'
-columns = ['data']
-data = pd.read_csv(os.path.join(datadir, 'data.csv'), names = columns)
-data = data.drop(0)
-data['data']
-pd.set_option('display.max_colwidth', -1)
-print(data.head())
-
-
-def allow_memory_growth():
-  gpus = tf.config.experimental.list_physical_devices('GPU')
-  if gpus:
-    try:
-      # Currently, memory growth needs to be the same across GPUs
-      for gpu in gpus:
-        tf.config.experimental.set_memory_growth(gpu, True)
-      logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-      print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    except RuntimeError as e:
-      # Memory growth must be set before GPUs have been initialized
-      print(e)
-
-def load_params(datadir, df):
-  image_path = []
-  velocity = []
-  steering = []
-  for i in range(len(data)):
-    indexed_data = data.iloc[i]
-    center = indexed_data[0]
-    splt = center.split(";")
-    name = imagedir + splt[0] + ".jpg"
-    image_path.append(os.path.join(datadir, name))
-    vel = float(splt[1])/120
-    steer = float(splt[2])
-    velocity.append(vel)
-    steering.append(steer)
-  image_paths = np.asarray(image_path)
-  velocities = np.asarray(velocity)
-  steerings = np.asarray(steering)
-
-  return image_paths, velocities, steerings
-
-allow_memory_growth()
-image_paths, velocities, steerings = load_params(datadir, data)
-
-param = []
-for it, i in enumerate(velocities):
-  param.append([velocities[it], steerings[it]])
-params = np.asarray(param)
-
-def img_preprocess(img):
-  img = npimg.imread(img)
-  img = img[380:600, :, :]
-  img = cv2.resize(img, (200, 200))
-  return img
-
-X_train, X_valid, Y_train, Y_valid = train_test_split(image_paths, params, test_size=0.2, random_state=0)
-X_train = np.array(list(map(img_preprocess, X_train)))
-X_valid = np.array(list(map(img_preprocess, X_valid)))
 
 
 def nvidia_model():
@@ -118,21 +53,90 @@ def nvidia_model():
   return model
 
 
+def allow_memory_growth():
+  gpus = tf.config.experimental.list_physical_devices('GPU')
+  if gpus:
+    try:
+      # Currently, memory growth needs to be the same across GPUs
+      for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+      logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+      print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+      # Memory growth must be set before GPUs have been initialized
+      print(e)
+      
+      
+def load_params(imagedir, data):
+  image_path = []
+  velocity = []
+  steering = []
+  for i in range(len(data)):
+    indexed_data = data.iloc[i]
+    center = indexed_data[0]
+    splt = center.split(";")
+    name = splt[0] + ".jpg"
+    image_path.append(os.path.join(imagedir, name))
+    vel = float(splt[1])/120
+    steer = float(splt[2])
+    velocity.append(vel)
+    steering.append(steer)
+  image_paths = np.asarray(image_path)
+  velocities = np.asarray(velocity)
+  steerings = np.asarray(steering)
+
+  return image_paths, velocities, steerings
 
 
-model = nvidia_model()
-print(model.summary())
+def img_preprocess(img):
+  img = npimg.imread(img)
+  img = img[380:600, :, :]
+  img = cv2.resize(img, (200, 200))
+  return img
 
 
+def main(arg):
+  imagedir = arg.images_path
+  csvdir = arg.labels_path
+  columns = ['data']
+  data = pd.read_csv(os.path.join(csvdir, arg.labels_name), names = columns)
+  data = data.drop(0)
+  data['data']
+  pd.set_option('display.max_colwidth', -1)
+  print(data.head())
+  allow_memory_growth()
+  image_paths, velocities, steerings = load_params(imagedir, data)
 
-print(Y_train)
-# history = model.fit(X_train, Y_train, epochs=5, validation_data=(X_valid, Y_valid), batch_size=128, verbose=1, shuffle=1)
-history = model.fit(X_train, Y_train,
-                    batch_size=32,
-                    epochs=8,
-                    verbose=1,
-                    validation_data=(X_valid, Y_valid))
-
-model.save('/home/tasoth/av_ws/src/av_07/scripts/model10.model')
+  param = []
+  for it, i in enumerate(velocities):
+    param.append([velocities[it], steerings[it]])
+  params = np.asarray(param)
 
 
+  X_train, X_valid, Y_train, Y_valid = train_test_split(image_paths, params, test_size=0.2, random_state=0)
+  X_train = np.array(list(map(img_preprocess, X_train)))
+  X_valid = np.array(list(map(img_preprocess, X_valid)))
+
+  model = nvidia_model()
+  print(model.summary())
+  history = model.fit(X_train, Y_train,
+                      batch_size=32,
+                      epochs=8,
+                      verbose=1,
+                      validation_data=(X_valid, Y_valid))
+  print(history)
+  model.save(arg.model_path + arg.model_name + '.model')
+
+  
+if __name__ == "__main__":
+    allow_memory_growth()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--images_path', type=str, help='Path to train images', required=True)
+    parser.add_argument('--labels_path', type=str, help='Path to csv file', required=True)
+    parser.add_argument('--labels_name', type=str, help='Name of csv file', required=True)
+    parser.add_argument('--model_path', type=str, help='Path to save the model', required=True)
+    parser.add_argument('--model_name', type=str, default='model', help='Name of the generated model', required=False)
+
+    args, _ = parser.parse_known_args()
+    main(args)
